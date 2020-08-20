ba6e335984d2b8888d319aacddf61ae6597129ae
Gergő Gazda <info@gazdagergo.com>
2020-08-20 07:33:28 +0200

    delete base and collection folders

--- src/Basic/Cell/index.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FC } from "react";
-
-const Cell: FC = ({ children }) => <td>{children}</td>;
-
-export default Cell;
--- src/Basic/TableHeadCell/index.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FC } from "react";
-
-const TableHeadCell: FC = ({ children }) => <th>{children}</th>;
-
-export default TableHeadCell;
